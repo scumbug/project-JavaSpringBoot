05278f54c2176a8c635806c2c9ba39a2ea21f168
xong002 <xong002@gmail.com>
2023-03-23 00:46:53 +0800

    add kotlin test

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
@@ -2,10 +2,13 @@ package sg.edu.ntu.m3project.m3project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
